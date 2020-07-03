1920f708ebff1647ee8cf91bc3bdb2afa77aee6d
Olga <ipvola@gmail.com>
2020-07-02 22:58:21 -0400

    dropDowns

--- src/test/java/e/links_and_drop_downs/LinksTest.java
+++ src/test/java/e/links_and_drop_downs/LinksTest.java
@@ -1,7 +1,6 @@
-package e.links;
+package e.links_and_drop_downs;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
